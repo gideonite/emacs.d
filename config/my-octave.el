f46ea1f0c4ff286365afb98bb9353e1b4cafec75
Gideon Dresdner <gideond@gmail.com>
2015-09-10 14:47:48 -0400

    remap all window changing keys in octave mode
    overkill? I dunno but it works.

--- config/my-octave.el
+++ config/my-octave.el
@@ -8,18 +8,21 @@
             (if (eq window-system 'x)
                 (font-lock-mode 1))))
 
-(eval-after-load 'octave-mode
-  (progn
-    (evil-set-initial-state 'octave-mode 'normal)
-    (evil-define-key 'normal octave-mode-map
-      (kbd "<f1>") 'octave-help 
-      (kbd "<f2>") 'octave-lookfor)))
-
-(eval-after-load 'inferior-octave-mode-map
+(defun keymap-forall-octave-modes ()
   (progn
     (evil-set-initial-state 'inferior-octave-mode 'normal)
     (evil-define-key 'normal inferior-octave-mode-map
+      (kbd "C-j") 'evil-window-down
+      (kbd "C-k") 'evil-window-up
+      (kbd "C-h") 'evil-window-left
+      (kbd "C-;") 'evil-window-right
       (kbd "<f1>") 'octave-help 
       (kbd "<f2>") 'octave-lookfor)))
+
+(eval-after-load 'octave-mode
+  (keymap-forall-octave-modes))
+
+(eval-after-load 'inferior-octave-mode-map
+  (keymap-forall-octave-modes))
 
 (provide 'my-octave)
