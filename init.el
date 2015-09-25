a5eaec7413760b55961857206b3f528999c4a2a3
Gideon Dresdner <gideond@gmail.com>
2015-09-25 10:34:58 -0400

    try added iedit by scope

--- init.el
+++ init.el
@@ -90,6 +90,25 @@
 
 (require 'my-python)
 
+;; source: https://www.masteringemacs.org/article/iedit-interactive-multi-occurrence-editing-in-your-buffer
+;; edit variables by scope, not by string matching
+(defun iedit-dwim (arg)
+  "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
+  (interactive "P")
+  (if arg
+      (iedit-mode)
+    (save-excursion
+      (save-restriction
+        (widen)
+        ;; this function determines the scope of `iedit-start'.
+        (if iedit-mode
+            (iedit-done)
+          ;; `current-word' can of course be replaced by other
+          ;; functions.
+          (narrow-to-defun)
+          (iedit-start (current-word) (point-min) (point-max)))))))
+(global-set-key (kbd "C-:") 'iedit-dwim)
+
 (setq exec-path (append exec-path '("/usr/bin/pdflatex")))
 
 (savehist-mode 1)
