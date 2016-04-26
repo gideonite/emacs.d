2adafc857bf9c89b6ff3dc4450b89fac805fbbe2
Gideon Dresdner <gideond@gmail.com>
2016-04-26 14:51:06 -0400

    fix modes for clojure

--- config/my-clojure.el
+++ config/my-clojure.el
@@ -6,10 +6,8 @@
 
 (use-package cider
   :ensure cider
-  :config (progn
-            ;; always eldoc in cider mode
-            (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-            (add-hook 'cider-mode-hook '(paredit-mode +1))
-            ))
+  :init (progn
+          (paredit-mode 1)
+          (electric-pair-mode 1)))
 
 (provide 'my-clojure)
